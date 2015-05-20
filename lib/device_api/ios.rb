feb4b3fabceb054ff1038c37fdb66ec02b713680
Jon Wilson <jon.wilson01@bbc.co.uk>
2015-05-20 12:17:54 +0100

    Changes to reflect functionality in the Android gem

--- lib/device_api/ios.rb
+++ lib/device_api/ios.rb
@@ -5,12 +5,21 @@ require 'device_api/ios/idevice'
 module DeviceAPI
   module IOS
 
+    # Returns an array of connected iOS devices
     def self.devices
       IDevice.devices.map do |d|
         if d.keys.first
-          DeviceAPI::IOS::Device.new(serial: d.keys.first, display: d.values.flatten.first, state: 'ok', type: 'Device')
+          DeviceAPI::IOS::Device.new(serial: d.keys.first, display: d.values.flatten.first, state: 'ok')
         end
       end
+    end
+
+    # Retrieve a Device object by serial ID
+    def self.device(serial)
+      if serial.to_s.empty?
+        raise DeviceAPI::BadSerialString.new("Serial was '#{ serial.nil? ? 'nil' : serial }'")
+      end
+      DeviceAPI::IOS::Device.new(serial: serial, state: 'ok')
     end
   end
 end
