a52406fcdd426c5e9e54467240553be0033c45a4
Jon Wilson <jon.wilson01@bbc.co.uk>
2016-02-10 15:10:44 +0000

    Added commands for zipping up the payload

--- lib/device_api/ios/signing.rb
+++ lib/device_api/ios/signing.rb
@@ -43,11 +43,34 @@ module DeviceAPI
         cert          = options[:cert]
         entitlements  = options[:entitlements]
         app           = options[:app]
+        original_app  = nil
 
-        result = execute("codesign --force --sign #{cert} --entitlements #{entitlements} '#{app}'")
+        if is_ipa?(app)
+          original_app = app
+          app = unpack_ipa(app)
+        end
+
+        # Check to see if the entitlements passed in is a file or the XML
+        unless File.exists?(entitlements)
+          file = Tempfile.new('entitlements')
+          file.write(entitlements)
+          file.close
+          entitlements = file.path
+        end
+        result = execute("codesign --force --sign '#{cert}' --entitlements #{entitlements} '#{app}'")
 
         raise SigningCommandError.new(result.stderr) if result.exit != 0
 
+        if original_app
+          zip_app(app, original_app)
+        end
+      end
+
+      def self.zip_app(payload_path, ipa_path)
+        result = execute("cd #{File.dirname(payload_path)}; zip -r #{ipa_path} ../Payload ")
+        raise SigningCommandError.new(result.stderr) if result.exit != 0
+
+        true
       end
 
       def self.get_signing_certs
@@ -82,12 +105,11 @@ module DeviceAPI
       end
 
       def self.enable_get_tasks(app_path)
-        entitlements = get_entitlements(app_path)
+        entitlements = get_entitlements(app_path, raw: true)
 
-        return true if entitlements['get-task-allow'] == "true"
+        return entitlements if entitlements.scan(/<key>get-task-allow<\/key>\\n.*<true\/>/).count > 0
 
-        xml = get_entitlements(app_path, raw: true)
-        xml.gsub('<false/>', '<true/>')
+        entitlements.gsub('<false/>', '<true/>') if entitlements.scan(/<key>get-task-allow<\/key>\\n.*<false\/>/)
       end
     end
 
